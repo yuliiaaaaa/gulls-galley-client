1cb968bb04f8cd02bf13398cf53baef4cd5147fb
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-09-07 18:05:23 +0300

    + added floor cushion

---
+++ src/components/floor-cushion/FloorCusgion.tsx
@@ -0,0 +1,19 @@
+import { ExploreButton } from '../utils/explore-button/ExploreButton';
+import s from './floorCushion.module.scss';
+
+export const FloorCushion = () => {
+  return (
+    <section className={s.cushion}>
+      <div className={`${s.container} ${s.cushion__container}`}>
+        <img src="/floor-cushion/floor-cushion.png" className={s.cushion__img} alt="cushion" />
+        <div className={s.cushion__explore_section}>
+          <h3 className={s.cushion__title}>Floor Cushion</h3>
+          <div className={s.cushion__exploreBtn}>
+            <h6 className={s.cushion__exploreBtnText}>Explore</h6>
+            <ExploreButton width={17} height={17} classNameIcon={s.cushion__explore_icon} className={s.cushion__explore_btn}/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
