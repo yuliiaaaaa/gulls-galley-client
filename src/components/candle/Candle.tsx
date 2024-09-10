584f84f7497f800bec6b0f2a4937c2faebd0087d
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-09-11 00:50:10 +0300

    + added animation on the buttons explore

--- src/components/candle/Candle.tsx
+++ src/components/candle/Candle.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { ExploreButton } from '../utils/explore-button/ExploreButton';
 import s from './Candle.module.scss';
 
@@ -13,18 +14,20 @@ export const Candle = () => {
           <img src="/candles/candle1.png" className={s.candle__img1} alt="candle" />
           <div className={s.candle__info}>
             <div className={s.candle__exploreBtn}>
-              <h6 className={s.candle__exploreBtnText}>Explore</h6>
-              <ExploreButton
-                width={17}
-                height={17}
-                classNameIcon={s.candle__explore_icon}
-                className={s.candle__explore_btn}
-              />
+              <Link to="#" className={s.candle__exploreBtn_block}>
+                <h6 className={s.candle__exploreBtnText}>Explore</h6>
+                <ExploreButton
+                  width={17}
+                  height={17}
+                  classNameIcon={s.candle__explore_icon}
+                  className={s.candle__explore_btn}
+                />
+              </Link>
             </div>
             <p className={s.candle__title}>Seaweed Candle</p>
           </div>
         </div>
-        
+
         <img src="/candles/candle2.png" alt="candle" />
       </div>
     </section>
