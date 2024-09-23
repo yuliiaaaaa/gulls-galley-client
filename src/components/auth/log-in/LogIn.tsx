36ae0ba232f808ac12ec1f4c7f7e3dc71a4f9ad0
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-09-23 22:45:44 +0300

    + added login form

--- src/components/auth/log-in/LogIn.tsx
+++ src/components/auth/log-in/LogIn.tsx
@@ -1,8 +1,16 @@
+import { Field, Form, Formik } from 'formik';
 import { AppRoute } from '../../../libs/enum/app-route-enum';
 import { LinkComponent } from '../../utils/link/Link';
 import s from './login.module.scss';
+import { LogInValidationSchema } from '../../../libs/validation-schemas/log-in-validation-shema';
+import { Button } from '../../utils/button/Button';
+import cn from 'classnames';
 
 export const LogIn = () => {
+  const handleSubmit = () => {};
+
+  const initialValues = { email: '', password: '' };
+
   return (
     <div>
       <div className={s.login}>
@@ -10,6 +18,34 @@ export const LogIn = () => {
           <h4 className={s.login__title}>LOGIN</h4>
           <p className={s.login__tip}>Enter your email and password to login</p>
         </div>
+
+        <Formik initialValues={initialValues} validationSchema={LogInValidationSchema} onSubmit={handleSubmit}>
+          {({ errors, touched, setFieldValue, isValid, dirty }) => (
+            <Form className={s.form}>
+              <div className={s.input__block}>
+                <Field
+                  className={cn(s.input, { [s.error__input]: errors.email && touched.email })}
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                />
+                {errors.email && touched.email ? <div className={s.error}>{errors.email}</div> : null}
+              </div>
+
+              <div className={s.input__block}>
+                <Field
+                  className={cn(s.input, { [s.error__input]: errors.password && touched.password })}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                {errors.password && touched.password ? <div className={s.error}>{errors.password}</div> : null}
+              </div>
+
+              <Button className={s.button} isDisabled={!isValid || dirty} title="Create account" />
+            </Form>
+          )}
+        </Formik>
 
         <div className={s.login__footer}>
           <p className={s.login__tip_footer}>Don’t have an account?</p>
--- src/components/utils/layout/Layout.tsx
+++ src/components/utils/layout/Layout.tsx
@@ -4,15 +4,17 @@ import { Header } from '../../header/Header';
 import ScrollToTop from '../../../libs/helpers/scrollTop';
 import { useIsAuthPage } from '../../../libs/hooks/useIsAuthPage';
 import { AppRoute } from '../../../libs/enum/app-route-enum';
+import s from './layout.module.scss';
+import cn from 'classnames';
 
 export const Layout = () => {
   const isFooterNotShown = useIsAuthPage([AppRoute.SIGN_UP, AppRoute.LOG_IN]);
-  
+
   return (
     <>
       <ScrollToTop />
       <Header />
-      <main>
+      <main className={cn(s.main, { [s.main__auth]: isFooterNotShown })}>
         <Outlet />
       </main>
       {!isFooterNotShown && <Footer />}
