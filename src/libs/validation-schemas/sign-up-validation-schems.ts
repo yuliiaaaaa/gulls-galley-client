8c4f44b4e9cb8cb74a77a71b2a589ae340ba1947
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-09-23 17:17:59 +0300

    added validation schema for log in/sign up

---
+++ src/libs/validation-schemas/sign-up-validation-schems.ts
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+import { ValidationMessages } from '../enum/ValidationMessages';
+import { MIN_PASSWORD_LENGTH, emailRegex } from '../consts/app';
+
+export const SignInValidationSchema = yup.object().shape({
+  firstName: yup.string().required(ValidationMessages.REQUIRED),
+  lastName: yup.string().required(ValidationMessages.REQUIRED),
+  email: yup
+    .string()
+    .required(ValidationMessages.REQUIRED)
+    .matches(emailRegex, ValidationMessages.EMAIL_FORMAT_NOT_CORRECT),
+  password: yup
+    .string()
+    .required(ValidationMessages.REQUIRED)
+    .min(MIN_PASSWORD_LENGTH, ValidationMessages.PASSWORD_LENGTH_MIN),
+});
