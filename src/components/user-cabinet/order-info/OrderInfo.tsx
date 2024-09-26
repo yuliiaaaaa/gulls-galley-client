20d6120a73be9a221e56927561f56e3d9b4208ca
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-09-26 15:46:30 +0300

    +added order and address no elements warning

--- src/components/user-cabinet/order-info/OrderInfo.tsx
+++ src/components/user-cabinet/order-info/OrderInfo.tsx
@@ -1,7 +1,29 @@
+import { AppRoute } from '../../../libs/enum/app-route-enum';
+import { Button } from '../../utils/button/Button';
+import { LinkComponent } from '../../utils/link/Link';
+import s from './orderInfo.module.scss';
+
 export const OrderInfo = () => {
+  const orders = [];
   return (
-    <div>
-      <h1>Order</h1>
+    <div className={s.orders}>
+      <h1 className={s.orders__title}>Orders</h1>
+
+      {!orders.length && (
+        <div className={s.orders__block}>
+          <p className={s.orders__text}>You haven’t placed any orders yet.</p>
+
+          <div className={s.orders__link_block}>
+            <LinkComponent
+              children="Continue shopping"
+              to={AppRoute.CATALOG}
+              router={true}
+              className={s.orders__link}
+            />
+            
+          </div>
+        </div>
+      )}
     </div>
   );
 };
