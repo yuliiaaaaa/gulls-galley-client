edb4b59f9f5d7904ccfce1ab3be46355cec3e35a
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-10-16 14:02:56 +0300

    + added adaptive design and positive scenario of deleting items

--- src/components/cart/cart-list/CartList.tsx
+++ src/components/cart/cart-list/CartList.tsx
@@ -4,13 +4,14 @@ import s from './CartList.module.scss';
 
 type Props = {
   items: CartItem[];
+  setCartItems: React.Dispatch<React.SetStateAction<CartItem[]>>;
 };
 
-export const CartList: React.FC<Props> = ({ items }) => {
+export const CartList: React.FC<Props> = ({ items,setCartItems }) => {
   return (
     <div className={s.list}>
       {items.map((item) => (
-        <Cartcard key={item.product.id} item={item} />
+        <Cartcard key={item.product.id} item={item} setCartItems={setCartItems} cartItems={items}/>
       ))}
     </div>
   );
